d9d887aec0e24f8a529af41382b4fb51c2a60c9f
Riz <55377396+Riz-0@users.noreply.github.com>
2025-03-15 17:30:11 +0800

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // Split the string
+    let strArray = str.split('')
+    // Reverse the array
+    let strArrayReverse = [];
+    for (let i = -1; i > 0 - strArray.length - 1; i--) {
+        strArrayReverse.push(strArray.at(i));
+    }
+    // Join the string
+    return strArrayReverse.join('');
 
+    // NOTE TO SELF: str.split("").reverse().join(""); works too. e.e
 };
 
 // Do not edit below this line
