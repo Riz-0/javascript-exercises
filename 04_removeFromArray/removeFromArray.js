ba5016ee4d161c8cf0e33f636f5fd90383876d82
Riz <55377396+Riz-0@users.noreply.github.com>
2025-03-16 16:44:15 +0800

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // Find index of something
+    for (let i = 0; i < args.length; i++) {
+        while (arr.indexOf(args[i]) > -1) {
+            arr.splice(arr.indexOf(args[i]), 1);
+        }
+    }
+    // If it's greater than -1, delete
+    // Else return
+    return arr;
 };
 
 // Do not edit below this line
