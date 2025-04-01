b223ff98f4ad8d5f99cf718af7bd50aef1ff4241
Riz <55377396+Riz-0@users.noreply.github.com>
2025-04-01 17:28:17 +0800

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str
+    .toLowerCase()
+    .split("")
+    .filter(
+      (char) => char !== "!" && char !== "," && char !== "." && char !== " "
+    )
+    .join("");
+  let reversed = str.split("").reverse().join("");
+  return str === reversed;
 };
 
 // Do not edit below this line
