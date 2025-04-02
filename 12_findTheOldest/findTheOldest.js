5cdb562b8660ce969c048ad38a40bb5d0d020bda
Riz <55377396+Riz-0@users.noreply.github.com>
2025-04-02 15:10:32 +0800

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  // First Try:
+  // people.sort((first, second) => {
+  //   if (first.yearOfDeath === undefined)
+  //     first.yearOfDeath = new Date().getFullYear();
+  //   if (second.yearOfDeath === undefined)
+  //     second.yearOfDeath = new Date().getFullYear();
+  //   return first.yearOfDeath - first.yearOfBirth >
+  //     second.yearOfDeath - second.yearOfBirth
+  //     ? -1
+  //     : 1;
+  // });
+  // return people[0];
+  return people.reduce((oldest, current) => {
+    return getAge(oldest) < getAge(current) ? current : oldest;
+  });
 };
+
+function getAge(person) {
+  person.yearOfDeath ??= new Date().getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
